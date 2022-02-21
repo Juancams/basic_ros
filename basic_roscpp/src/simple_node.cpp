23a53e04d980e4507e19772dbad0966016aae142
Juancams <jc.manzanares.serrano@gmail.com>
2022-02-21 13:56:19 +0100

    Test lint

--- basic_roscpp/src/simple_node.cpp
+++ basic_roscpp/src/simple_node.cpp
@@ -2,11 +2,10 @@
 
 #include "ros/ros.h"
 
-int main(int argc, char **argv)    {
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "simple_node");
-    ros::NodeHandle n;    
-
-
+    ros::NodeHandle n;
 
     ros::spin();
 
